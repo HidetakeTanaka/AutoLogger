7c11ab7b1a22c4ef04db859a05a3976b5a6e68fd
Farjana Akter <farjanaabedin858@gmail.com>
2025-11-25 18:11:15 +0100

    script24.py

---
+++ scripts/script24.py
@@ -0,0 +1,16 @@
+# script24.py
+def is_palindrome(s):
+    cleaned = "".join(ch.lower() for ch in s if ch.isalnum())
+    return cleaned == cleaned[::-1]
+
+
+def main():
+    word = "Racecar"
+    if is_palindrome(word):
+        print("Palindrome")
+    else:
+        print("Not palindrome")
+
+
+if __name__ == "__main__":
+    main()
